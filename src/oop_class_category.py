9ed7473f9fab31ef9bbf08caeca7d9e4086d4c71
ilya <ilifud98@mail.ru>
2025-05-15 13:26:15 +0700

    edit class category & product

--- src/oop_class_category.py
+++ src/oop_class_category.py
@@ -5,9 +5,25 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.product_count += len(products) if products else 0
         Category.category_count += 1
+
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
+
+
+
+        
